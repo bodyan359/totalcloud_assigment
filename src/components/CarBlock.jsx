6f0d69d6cde63078b161a63775561eba9cc55d65
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-02-06 21:25:58 +0100

    Rating UP Realtime

--- src/components/CarBlock.jsx
+++ src/components/CarBlock.jsx
@@ -1,39 +1,52 @@
-import React from 'react';
+import React from "react";
 
-function CarBlock({ make, model, year, submittedBy, imageUrl, more, rating}) {
-
-    // const [rrating, setNewRating] = React.useState(1);
-    console.log(rating);
-
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <img className="car-block__image img-fluid" src={imageUrl} alt={make + " " + model + " " + year} />
-                    </div>
-                    <div className="col">
-                        <div className="row">
-                            <div className="col">
-                              <img className="w-25 img-thumbnail" src="https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png" 
-                              alt="RatignUpButton" onClick={() => 2} />                        
-                            </div>
-                            <div className="col">
-                                <p className="car-block__rating font-weight-bold"> {rating}</p>
-                            </div>
-                        </div>
-
-                        <a href={more}>{model}</a>
-                        <p>{make + ", "+year}</p>
-                        <div className="row">
-                        <p className="col-md2 text-secondary">Submitted by: {" "}</p> 
-                        <p className="col-md">{submittedBy}</p>
-                        </div>
-                    </div>
-                </div>
+function CarBlock({
+  make,
+  model,
+  year,
+  submittedBy,
+  imageUrl,
+  more,
+  rating,
+  addRating
+}) {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img
+              className="car-block__image img-fluid"
+              src={imageUrl}
+              alt={make + " " + model + " " + year}
+            />
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <img
+                  className="w-25 img-thumbnail"
+                  src="https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png"
+                  alt="RatignUpButton"
+                  onClick={addRating}
+                />
+              </div>
+              <div className="col">
+                <p className="car-block__rating font-weight-bold"> {rating}</p>
+              </div>
             </div>
+
+            <a href={more}>{model}</a>
+            <p>{make + ", " + year}</p>
+            <div className="row">
+              <p className="col-md2 text-secondary">Submitted by: </p>
+              <p className="col-md">{submittedBy}</p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default CarBlock;
--- src/components/CarsItem.jsx
+++ src/components/CarsItem.jsx
@@ -1,47 +1,68 @@
-import React from 'react';
-import CarBlock from './CarBlock';
+import React from "react";
+import CarBlock from "./CarBlock";
 
 let dummydb = {
-    "cars": [
-      {
-        "id":1,
-        "make": "Mercedes-Benz",
-        "model": "W211",
-        "year": "2002",
-        "submittedBy": "Bogdan",
-        "imageUrl": "https://www.auto-data.net/images/f23/Mercedes-Benz-E-class-W211.jpg",
-        "more":"https://www.auto-data.net/en/mercedes-benz-e-class-w211-amg-e-55-v8-476hp-automatic-12905",
-        "rating": 17
-      },      {
-        "id":2,
-        "make": "BMW",
-        "model": "E60",
-        "year": "2004",
-        "submittedBy": "Romeo",
-        "imageUrl": "https://www.auto-data.net/images/f52/BMW-5-Series-E60-Facelift-2007.jpg",
-        "more":"https://www.auto-data.net/en/bmw-5-series-e60-facelift-2007-generation-5341",
-        "rating": 7
-      },      {
-        "id":3,
-        "make": "BMW",
-        "model": "G30",
-        "year": "2020",
-        "submittedBy": "Ivan",
-        "imageUrl": "https://www.auto-data.net/images/f83/BMW-5-Series-Sedan-G30-LCI-facelift-2020_1.jpg",
-        "more":"https://www.auto-data.net/en/bmw-5-series-sedan-g30-lci-facelift-2020-generation-7813",
-        "rating": 14
-      }
-    ]
-  }
+  cars: [
+    {
+      id: 1,
+      make: "Mercedes-Benz",
+      model: "W211",
+      year: "2002",
+      submittedBy: "Bogdan",
+      imageUrl:
+        "https://www.auto-data.net/images/f23/Mercedes-Benz-E-class-W211.jpg",
+      more:
+        "https://www.auto-data.net/en/mercedes-benz-e-class-w211-amg-e-55-v8-476hp-automatic-12905",
+      rating: 17
+    },
+    {
+      id: 2,
+      make: "BMW",
+      model: "E60",
+      year: "2004",
+      submittedBy: "Romeo",
+      imageUrl:
+        "https://www.auto-data.net/images/f52/BMW-5-Series-E60-Facelift-2007.jpg",
+      more:
+        "https://www.auto-data.net/en/bmw-5-series-e60-facelift-2007-generation-5341",
+      rating: 7
+    },
+    {
+      id: 3,
+      make: "BMW",
+      model: "G30",
+      year: "2020",
+      submittedBy: "Ivan",
+      imageUrl:
+        "https://www.auto-data.net/images/f83/BMW-5-Series-Sedan-G30-LCI-facelift-2020_1.jpg",
+      more:
+        "https://www.auto-data.net/en/bmw-5-series-sedan-g30-lci-facelift-2020-generation-7813",
+      rating: 14
+    }
+  ]
+};
 
 function CarsItem() {
-    return (
-        <div className="container">
-        {[...dummydb.cars].sort((a,b) => {return b.rating - a.rating}).map((car) => (
-          <CarBlock key={car.id} {...car} />
+  const [data, setData] = React.useState(dummydb.cars);
+  const increaseRating = (id) =>
+    setData((oldData) =>
+      oldData.map((car) =>
+        car.id === id ? { ...car, rating: car.rating + 1 } : car
+      )
+    );
+  return (
+    <div className="container">
+      {[...data]
+        .sort((a, b) => b.rating - a.rating)
+        .map((car) => (
+          <CarBlock
+            key={car.id}
+            {...car}
+            addRating={() => increaseRating(car.id)}
+          />
         ))}
-        </div>
-    )
+    </div>
+  );
 }
 
 export default CarsItem;
